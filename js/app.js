72d2b259f9f9f369d51213a61cee6baae05a8ea4
Ahmed ZENAKHI <ahmed.zenakhi-prestataire@ca-gip.fr>
2019-04-09 07:38:40 +0200

    Dockerfile multistage

--- js/app.js
+++ js/app.js
@@ -2,12 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello tst')
+  res.send('Hello world')
 })
-
-app.get('/test', function (req, res) {
-    res.send('Test')
-  })
 
 app.listen(3000, function () {
     console.log('Example on 3000!')
--- python/app.py
+++ python/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World from Pyton Flsk'
+    return 'Hello World'
 
 app.run(host='0.0.0.0', port=3000, debug=True)
